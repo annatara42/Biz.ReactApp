935d33d12c3547ce8ae42608e4fe2bdc80d431af
annatara42 <annatara42@gmail.com>
2020-12-03 00:51:39 -0500

    Initial commit

---
+++ build/precache-manifest.26ba9e15939023fd1a4e85f9ff4e572f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bce1eb23c82c220543e0",
+    "url": "/static/css/main.24bcb12e.chunk.css"
+  },
+  {
+    "revision": "bce1eb23c82c220543e0",
+    "url": "/static/js/main.60c97fc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ef5995c5d962bc0b67a",
+    "url": "/static/css/2.436446d2.chunk.css"
+  },
+  {
+    "revision": "9ef5995c5d962bc0b67a",
+    "url": "/static/js/2.c872f759.chunk.js"
+  },
+  {
+    "revision": "3abc072b58309684d89818fb20b87690",
+    "url": "/index.html"
+  }
+];
